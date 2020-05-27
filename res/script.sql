13df0fbcb30f170e4df924de5ff9fdc71ae21b72
emilio <emendezduran@danielcastelao.org>
2020-05-27 19:04:10 +0200

    Crear tablas en la base de datos script de creacion de tablas

--- res/script.sql
+++ res/script.sql
@@ -1,53 +1,42 @@
--- sqlite pragma
-PRAGMA foreign_keys=1;
-
--- clientes
-create table clientes
-(
-    id        integer     not null
-        constraint clientes_pk
-            primary key autoincrement,
-    nombre    varchar2(12),
-    apellido1  varchar2(12),
-    apellido2 varchar2(12),
-    documento       varchar2(9) not null,
-    edad  int,
-    provincia varchar2(20)
-);
-
-create unique index clientes_dni_uindex
-    on clientes (documento);
-
-create unique index clientes_id_uindex
-    on clientes (id);
-
-
--- productos
-create table productos
-(
-    id          integer not null
-        constraint productos_pk
-            primary key autoincrement,
-    nombre      varchar2(12),
-    precio      integer,
-    stock       integer,
-    categoria_id integer
-);
-
-create unique index productos_id_uindex
-    on productos (id);
-
-
--- categorias
 create table categorias
 (
-     id          integer not null
+    categoria_id     INTEGER not null
         constraint categorias_pk
             primary key autoincrement,
-    nombre      varchar2(12),
+    categoria_nombre varchar
+);
+
+create unique index categorias_categoria_id_uindex
+    on categorias (categoria_id);
+
+create table clientes
 (
+    cliente_id         INTEGER not null
+        constraint Clientes_pk
+            primary key autoincrement,
+    cliente_nombre     varchar,
+    cliente_apellido_1 varchar,
+    cliente_apellido_2 varchar,
+    cliente_documento  int,
+    cliente_edad       int,
+    cliente_provincia  varchar
+);
 
-create index categorias_id_index
-    on categorias (id);
+create unique index Clientes_cliente_documento_uindex
+    on clientes (cliente_documento);
 
+create unique index Clientes_cliente_id_uindex
+    on clientes (cliente_id);
 
+create table productos
+(
+    producto_id     INTEGER not null,
+    producto_nombre varchar,
+    producto_precio int,
+    producto_stock  int,
+    categoria_id    INTEGER
+        references categorias
+);
+
+create unique index productos_producto_id_uindex
+    on productos (producto_id);
