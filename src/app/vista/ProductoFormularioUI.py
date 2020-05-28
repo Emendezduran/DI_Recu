07af44dfa45785e02a1351b63f36b3f0d9163df7
emilio <emendezduran@danielcastelao.org>
2020-05-28 16:59:09 +0200

    Rellenar combobox de categorias

--- src/app/vista/ProductoFormularioUI.py
+++ src/app/vista/ProductoFormularioUI.py
@@ -1,5 +1,6 @@
 import gi
 
+from app.datos import CategoriaDao
 from src.app.modelo.Producto import Producto
 
 gi.require_version('Gtk', '3.0')
@@ -24,7 +25,9 @@ class ProductoFormularioUI(Gtk.Window):
         signals = {
             "btn_volver_act": self.on_btn_volver,
             "btn_limpiar_act": self.on_btn_limpiar,
-            "btn_guardar_act": self.on_btn_guardar
+            "btn_guardar_act": self.on_btn_guardar,
+            "cb_cambio_act": self.on_cb_cambio
+
         }
         builder.connect_signals(signals)
         self.box_ui = builder.get_object("box_ui")
@@ -33,14 +36,28 @@ class ProductoFormularioUI(Gtk.Window):
         self.producto_nombre_entry: Gtk.Entry = builder.get_object("producto_nombre_entry")
         self.producto_precio_entry: Gtk.Entry = builder.get_object("producto_precio_entry")
         self.producto_stock_entry: Gtk.Entry = builder.get_object("producto_stock_entry")
-        #self.producto_categoria_combo: Gtk.Entry = builder.get_object("producto_categoria_combo")
+
+        self.producto_categoria_combo = builder.get_object("producto_categoria_combo")
 
         self.producto_nombre_entry.set_text(self.producto.producto_nombre)
         self.producto_precio_entry.set_text(str(self.producto.producto_precio))
         self.producto_stock_entry.set_text(str(self.producto.producto_stock))
-        #self.producto_categoria_combo.set_text(self.producto.producto_categoria)
+
+        categorias = CategoriaDao.get_all()
+        lista_categorias_nombre = Gtk.ListStore(str)
+        for categoria in categorias:
+            nombre = categoria.categoria_nombre
+            lista_categorias_nombre.append([nombre])
+
+        self.producto_categoria_combo.set_model(lista_categorias_nombre)
+        renderer_text = Gtk.CellRendererText()
+        self.producto_categoria_combo.pack_start(renderer_text, True)
+        self.producto_categoria_combo.add_attribute(renderer_text, "text", 0)
 
         self.show_all()
+
+    def on_cb_cambio(self, obj):
+        pass
 
     def on_btn_volver(self, button):
         """volver a la lista de productos
